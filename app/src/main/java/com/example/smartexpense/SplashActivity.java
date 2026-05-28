6b3c99a05883d813ba5faf69763144e6958c2059
brianceasar <brianceasar7@gmail.com>
2026-05-28 11:48:02 +0300

    create splash screen

---
+++ app/src/main/java/com/example/smartexpense/SplashActivity.java
@@ -0,0 +1,35 @@
+package com.example.smartexpense;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Configure activity to cover the entire screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash);
+
+        // Handler thread with a 2000ms delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Move to MainActivity
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                // Clear the stack
+                finish();
+            }
+        }, 2000);
+    }
+}
